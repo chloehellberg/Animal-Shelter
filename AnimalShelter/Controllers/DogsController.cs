e4a488708f24ca2b3a290a1136b8fa722fd051fa
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-10-30 10:52:27 -0700

    Add full CRUD functionality to DogsController.

---
+++ AnimalShelter/Controllers/DogsController.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DogsController : ControllerBase
+  {
+    private AnimalShelterContext _db;
+
+    public DogsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/dogs
+    [HttpGet]
+    public ActionResult<IEnumerable<Dog>> Get(string breed, string gender, string name, int page, int size)
+    {
+      var query = _db.Dogs.AsQueryable();
+
+      if (breed != null)
+      {
+        query = query.Where(entry => entry.Breed == breed);
+      }
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (page != 0)
+      {
+        if (size !=0 )
+        {
+        }
+        else {
+          size = 2;
+        }
+        var entries = query.OrderBy(o => o.Name).Skip((page - 1) * size).Take(size).ToList();
+        query = entries.AsQueryable();
+      }
+
+      return query.ToList();
+    }
+
+    // POST api/dogs
+    [HttpPost]
+    public void Post([FromBody] Dog dog)
+    {
+      _db.Dogs.Add(dog);
+      _db.SaveChanges();
+    }
+
+    // GET api/dogs/4
+    [HttpGet("{id}")]
+    public ActionResult<Dog> Get(int id)
+    {
+      return _db.Dogs.FirstOrDefault(entry => entry.DogId == id);
+    }
+
+    // PUT api/dogs/4
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Dog dog)
+    {
+      dog.DogId = id;
+      _db.Entry(dog).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/dogs/4
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var dogToDelete = _db.Dogs.FirstOrDefault(entry => entry.DogId == id);
+      _db.Dogs.Remove(dogToDelete);
+      _db.SaveChanges();
+    }
+  }
+}
